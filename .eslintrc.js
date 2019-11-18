5d3f0ccafa9f190f8a1fab5a571065a91cfecfbb
zzcyrus <zzcyrus@outlook.com>
2019-11-18 12:02:20 +0800

    chore: update to vue/cli 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,22 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
-  plugins: ['vue'],
+  extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+      }
+    ]
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
-}
+};
--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,6 @@ module.exports = {
       }
     },
     module: {
-      unknownContextCritical: /^.\/.*$/,
       unknownContextCritical: false
     },
     plugins: plugins
